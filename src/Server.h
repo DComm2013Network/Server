5d3898b04a72be78b71ba1ee31e9340838bc71d8
Andrew Burian <andrew@burian.ca>
2014-01-29 17:48:51 -0800

    Added header files

---
+++ src/Server.h
@@ -0,0 +1,21 @@
+/*---------------------------------------------------------------------* 
+-- HEADER FILE: Server.h 		The definitions and declarations to be
+-- 								to be used to communicate between the
+-- 								components of the Server
+--
+-- PROGRAM:		Game-Server
+--
+-- FUNCTIONS:
+-- 		none
+--
+-- DATE: 		January 27, 2014
+--
+-- REVISIONS: 	none
+--
+-- DESIGNER: 	Andrew Burian
+--
+-- PROGRAMMER: 	Andrew Burian
+--
+-- NOTES:
+-- This is unique to the server
+----------------------------------------------------------------------*/
