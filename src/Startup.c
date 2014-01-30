ff72bce84cc554f4a0211871ce45b865fe9785ba
Andrew Burian <andrew@burian.ca>
2014-01-30 14:02:15 -0800

    main built

---
+++ src/Startup.c
@@ -0,0 +1,102 @@
+/*---------------------------------------------------------------------* 
+-- HEADER FILE: Startup.c 	The main entry point for the server process
+--							
+--
+-- PROGRAM:		Game-Server
+--
+-- FUNCTIONS:
+-- 		int main(int argc, char* argv[])
+--
+-- DATE: 		January 30, 2014
+--
+-- REVISIONS: 	none
+--
+-- DESIGNER: 	Andrew Burian
+--
+-- PROGRAMMER: 	Andrew Burian
+--
+-- NOTES:
+-- 
+----------------------------------------------------------------------*/
+
+#include "NetComm.h"
+#include "Server.h"
+
+#include <sys/socket.h>
+#define READ 0
+#define WRITE 1
+
+int main(int argc, char* argv[]){
+	SOCKET uiSockSet[2];
+	SOCKET connectionSockSet[2];
+	SOCKET generalSockSet[2];
+	SOCKET gameplaySockSet[2];
+	SOCKET outswitchSockSet[2];
+
+	if(socketpair(AF_UNIX, SOCK_STREAM, 0, uiSockSet) == -1){
+		printf(stderr, "Socket pair error: uiSockSet");
+		return -1;
+	}
+
+	// Start the UI process
+	if(fork() == 0){
+		UI(uiSockSet[WRITE]);
+		return 0;
+	}
+	close(uiSockSet[WRITE]);
+	// ----------------------------
+
+	if(socketpair(AF_UNIX, SOCK_STREAM, 0, outswitchSockSet) == -1){
+		printf(stderr, "Socket pair error: outswitchSockSet");
+		return -1;
+	}
+	if(socketpair(AF_UNIX, SOCK_STREAM, 0, connectionSockSet) == -1){
+		printf(stderr, "Socket pair error: connectionSockSet");
+		return -1;
+	}
+
+	// Start the Connection Manager
+	if(fork() == 0){
+		ConnectionManager(connectionSockSet[READ], outswitchSockSet[WRITE]);
+		return 0;
+	}
+	close(connectionSockSet[READ]);
+	// ----------------------------
+
+	if(socketpair(AF_UNIX, SOCK_STREAM, 0, generalSockSet) == -1){
+		printf(stderr, "Socket pair error: generalSockSet");
+		return -1;
+	}
+
+	// Start the General Controller
+	if(fork() == 0){
+		GeneralController(generalSockSet[READ], outswitchSockSet[WRITE]);
+		return 0;
+	}
+	close(generalSockSet[READ]);
+	// ----------------------------
+
+	if(socketpair(AF_UNIX, SOCK_STREAM, 0, gameplaySockSet) == -1){
+		printf(stderr, "Socket pair error: gameplaySockSet");
+		return -1;
+	}
+
+	// Start the Gameplay Controller
+	if(fork() == 0){
+		GameplayController(gameplaySockSet[READ], outswitchSockSet[WRITE]);
+		return 0;
+	}
+	close(gameplaySockSet[READ]);
+	// ----------------------------	
+
+	// Start the Outbound Switchboard
+	if(fork() == 0){
+		OutboundSwitchboard(outswitchSockSet[READ]);
+		return 0;
+	}
+	close(gameplaySockSet[READ]);
+	// ----------------------------		
+
+	InboundSwitchboard(connectionSockSet[WRITE], generalSockSet[WRITE], gameplaySockSet[WRITE], outswitchSockSet[WRITE]);
+	return 0;
+}
