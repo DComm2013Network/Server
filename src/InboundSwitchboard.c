ddb347a3e41c5d2a16d0292193605bb8b04911bf
Andrew Burian <andrew@burian.ca>
2014-02-14 16:18:34 -0800

    Laid out Inbound switch functions Honestly, more of a trial commit than anything else.

--- src/InboundSwitchboard.c
+++ src/InboundSwitchboard.c
@@ -1,25 +1,67 @@
-/*
- * InboundSwitchboard.c
- *
- *  Created on: 13 Feb 2014
- *      Author: chris
- */
+/*-------------------------------------------------------------------------------------------------------------------*
+-- SOURCE FILE: InboundSwitchboard.c 	
+--		The Process that will handle all traffic from already established client connections
+--
+-- FUNCTIONS:
+-- 		int InboundSwitchboard(SOCKET connectionSockSet, SOCKET generalSockSet, SOCKET gameplaySockSet, 
+--					SOCKET outswitchSockSet)
+--
+--
+-- DATE: 		February 14, 2014
+--
+-- REVISIONS: 	none
+--
+-- DESIGNER: 	Andrew Burian
+--
+-- PROGRAMMER: 	Andrew Burian
+--
+-- NOTES:
+-- 
+*-------------------------------------------------------------------------------------------------------------------*/
 
 
-/*
- * INBOUND SWITCHBOARD
-    allocate socket lists
-    while 1
-        listen on all sockets
-        if new connection
-            add new connection to socket to list of inbound sockets
-            pass socket+info to outbound switchboard
+#inlcude "NetComm.h"
+#include "Server.h"
 
-        if movement packet
-            pass to gameplay controller
+/*--------------------------------------------------------------------------------------------------------------------
+-- FUNCTION:	Main
+--
+-- DATE: 		February 4, 2014
+--
+-- REVISIONS: 	none
+--
+-- DESIGNER: 	Andrew Burian
+--
+-- PROGRAMMER: 	Andrew Burian
+--
+-- INTERFACE: 	int InboundSwitchboard(SOCKET connectionSockSet, SOCKET generalSockSet, SOCKET gameplaySockSet, 
+--					SOCKET outswitchSockSet)
+--
+-- RETURNS: 	int
+--					failure:	-99 Not yet implemented
+--					success: 	0
+--
+-- NOTES:
+-- 
+----------------------------------------------------------------------------------------------------------------------*/
+int InboundSwitchboard(SOCKET connectionSockSet, SOCKET generalSockSet, SOCKET gameplaySockSet, SOCKET outswitchSockSet){
+	/*
+	INBOUND SWITCHBOARD
+		allocate socket lists
+		while 1
+			listen on all sockets
+			if new connection
+				add new connection to socket to list of inbound sockets
+				pass socket+info to outbound switchboard
 
-        if game status packet
-            pass to game status controller
+			if movement packet
+				pass to gameplay controller
 
-            .... etc
- */
+			if game status packet
+				pass to game status controller
+
+				.... etc
+	 */
+	
+	return -99;
+}
