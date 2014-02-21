88e00e1803f8f1a412651d04cb5a17e71c5e7395
German <vo.german@gmail.com>
2014-02-20 21:29:11 -0800

    UI branch and some initial work

--- src/UI.c
+++ src/UI.c
@@ -22,6 +22,8 @@
 #include "NetComm.h"
 #include "Server.h"
 
+extern int RUNNING;
+
 /*--------------------------------------------------------------------------------------------------------------------
 -- FUNCTION:	...
 --
@@ -42,7 +44,48 @@
 -- NOTES:
 -- 
 ----------------------------------------------------------------------------------------------------------------------*/
-int UI(SOCKET outSock){
+int UI(SOCKET outSock) {
 	
+	PKT_SERVER_SETUP pkt;
+
+	// prompt setup info
+	const char* format = "%s %d %d";
+	printf("Enter serverName maxPlayers and port as %s\n", format);
+	if(scanf(format, pkt.serverName, &pkt.maxPlayers, &pkt.port) != 3) {
+		printf("Invalid arguments");
+		break;
+		
+		printf(format, pkt.serverName, pkt.maxPlayers, pkt.port);
+		printf("\n");
+	}
+
+	// pass IPC packet 0 to Inbound Switchboard		
+	send(outSock, pkt, sizeof(pkt), 0);
+
+	char input[
+    // while running
+	while(RUNNING)
+	{
+		// get input
+		scanf("%s", input);
+		
+        // if quit
+            // create quit packet
+            // send to switchboard
+            // exit
+		if(strcmp(input, UI_QUIT) == 0)
+		{
+			;//quitaction
+		}
+		
+		if(strcmp(
+	}
+    
+
+        
+
+        // if other
+            // other
+//	}
 	return -99;
 }
